fe5a17db31485602be8ae895b1e95eefb4c7552d
Keith <kg.anderson@outlook.com>
2021-02-05 11:50:27 +1300

    Add CalenderDate Widget

---
+++ client/components/CalenderDate.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+
+function CalenderDate () {
+  const [value, onChange] = useState(new Date())
+  return (
+    <>
+      <Calendar onChange={onChange} value={value}/>
+    </>
+  )
+}
+
+export default CalenderDate
