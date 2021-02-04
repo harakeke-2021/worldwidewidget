3adb3b8d059f377fb14b48598e7ff8e3a35cf65a
Kenneth Anderson <ken@kenneth.net.nz>
2021-02-05 11:43:55 +1300

    test requesting page and inserting new page

--- server/data/db.test.js
+++ server/data/db.test.js
@@ -13,4 +13,22 @@ describe('database interaction', () => {
         return null
       })
   })
+
+  it('can return a page by name', () => {
+    return db.getPageByName('kenneth')
+      .then(page => {
+        expect(page.name).toBe('kenneth')
+        return null
+      })
+  })
+
+  it('can insert a new page', async () => {
+    const inserted = await db.createPage('james')
+    expect(inserted.name).toBe('james')
+
+    db.datastores.pages.findOne({ name: 'james' }, (err, doc) => {
+      expect(doc.name).toBe('james')
+      if (err) throw err
+    })
+  })
 })
